7bbcbd961d4947bba610ebf0f31980c55a8724e1
fancyydev <davidfregosoleon12@gmail.com>
2024-07-19 23:48:16 -0600

    variable name correction

--- rpsls_game.py
+++ rpsls_game.py
@@ -15,24 +15,24 @@ transition_table = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
 ]
-tirada_anterior = None
-tirada_actual = None
-puntuacion_humano = 0
-puntuacion_maquina = 0
+current_game_turn = None
+previous_game_turn = None
+human_score = 0
+machine_score = 0
 
-def results(opcion_humano, opcion_maquina):
+def results(human_option, machine_option):
     #Si eligen lo mismo es empate
-    if(opcion_humano == opcion_maquina):
+    if(human_option == machine_option):
         return None
-    elif (opcion_humano == "piedra" and (opcion_maquina == "tijera" or opcion_maquina == "lagarto")):
+    elif (human_option == "piedra" and (machine_option == "tijera" or machine_option == "lagarto")):
         return True 
-    elif(opcion_humano == "papel" and (opcion_maquina == "piedra" or opcion_maquina == "spock")):
+    elif(human_option == "papel" and (machine_option == "piedra" or machine_option == "spock")):
         return True
-    elif(opcion_humano == "tijera" and (opcion_maquina == "papel" or opcion_maquina == "lagarto")):
+    elif(human_option == "tijera" and (machine_option == "papel" or machine_option == "lagarto")):
         return True
-    elif(opcion_humano == "lagarto" and (opcion_maquina == "spock" or opcion_maquina == "papel")):
+    elif(human_option == "lagarto" and (machine_option == "spock" or machine_option == "papel")):
         return True
-    elif(opcion_humano == "spock" and (opcion_maquina == "tijera" or opcion_maquina == "piedra")):
+    elif(human_option == "spock" and (machine_option == "tijera" or machine_option == "piedra")):
         return True
     else:
         return False
@@ -69,41 +69,41 @@ def tryWinMachine(possible_option):
 def get_max_indices(ls,value):
     return [i for i in range(len(ls)) if ls[i] == value]
 
-def get_choice(tirada = None, index = None):
-    if (tirada == "piedra" or index == 0):
+def get_choice(option = None, index = None):
+    if (option == "piedra" or index == 0):
         return 0, "piedra" 
-    elif(tirada == "papel" or index == 1):
+    elif(option == "papel" or index == 1):
         return 1, "papel"
-    elif(tirada == "tijera" or index == 2):
+    elif(option == "tijera" or index == 2):
         return 2, "tijera"
-    elif(tirada == "lagarto" or index == 3):
+    elif(option == "lagarto" or index == 3):
         return 3, "lagarto"
     else:
         return 4, "spock"
 
-def play(opcion_humano):
+def play(human_option):
     global transition_table
-    global tirada_anterior
-    global tirada_actual
-    global puntuacion_humano
-    global puntuacion_maquina
+    global current_game_turn
+    global previous_game_turn
+    global human_score
+    global machine_score
 
     ban = False
 
-    tirada_actual = opcion_humano
-    tirada_actual_index = get_choice(tirada=tirada_actual)[0]
+    previous_game_turn = human_option
+    previous_game_turn_index = get_choice(option=previous_game_turn)[0]
 
-    if (tirada_anterior == None):
-        tirada_anterior = tirada_actual
+    if (current_game_turn == None):
+        current_game_turn = previous_game_turn
         ban = True
 
     #Nos devuelve la fila de la matriz que le corresponde
-    #a la tirada anterior
-    tirada_anterior_index = get_choice(tirada=tirada_anterior)[0]
+    #a la option anterior
+    current_game_turn_index = get_choice(option=current_game_turn)[0]
     #Obtenemos el valor mas grande dentro de esa fila
-    max_value = max(transition_table[tirada_anterior_index])
+    max_value = max(transition_table[current_game_turn_index])
     #Si el valor maximo se repite obtenemos un arreglo con las posiciones del valor mas grande
-    index_max_values = get_max_indices(transition_table[tirada_anterior_index], max_value)
+    index_max_values = get_max_indices(transition_table[current_game_turn_index], max_value)
     #Como puede haber varias opciones con la misma probabilidad
     #Se escoje el indice de una al azar 
     machine_choice_index = index_max_values[random.randint(0,len(index_max_values)-1)]
@@ -112,7 +112,7 @@ def play(opcion_humano):
     machine_choice_name = tryWinMachine(machine_choice_name)
 
     #Configuramos la imagen de la opcion de el usuario 
-    img = Image.open(f"{opcion_humano}.png")
+    img = Image.open(f"{human_option}.png")
     img = img.resize((200, 200))
     img = ImageTk.PhotoImage(img)
     
@@ -127,23 +127,23 @@ def play(opcion_humano):
     img_robot.configure(image=img)
     img_robot.image=img
 
-    result_ban = results(opcion_humano, machine_choice_name)
+    result_ban = results(human_option, machine_choice_name)
     if result_ban == None:
-        msg_resultado.set("EMPATE")
+        msg_result.set("EMPATE")
     elif result_ban:
-        puntuacion_humano = puntuacion_humano + 1
-        msg_human.set(f"HUMANO: {puntuacion_humano}")
-        msg_resultado.set("GANASTE")
+        human_score = human_score + 1
+        msg_human.set(f"HUMANO: {human_score}")
+        msg_result.set("GANASTE")
     else:
-        puntuacion_maquina = puntuacion_maquina + 1
-        msg_maquina.set(f"MAQUINA: {puntuacion_maquina}")
-        msg_resultado.set("PERDISTE")
+        machine_score = machine_score + 1
+        msg_machine.set(f"MAQUINA: {machine_score}")
+        msg_result.set("PERDISTE")
     
-    #Guardamos dentro del renglon de tirada anterior lo elegido por el usuario para tener en cuenta su probabilidad
+    #Guardamos dentro del renglon de option anterior lo elegido por el usuario para tener en cuenta su probabilidad
     if (ban == False):
-        transition_table[tirada_anterior_index][tirada_actual_index] = transition_table[tirada_anterior_index][tirada_actual_index] + 1 
+        transition_table[current_game_turn_index][previous_game_turn_index] = transition_table[current_game_turn_index][previous_game_turn_index] + 1 
 
-    tirada_anterior = tirada_actual
+    current_game_turn = previous_game_turn
 
 
 
@@ -158,13 +158,13 @@ msg_human = StringVar()
 msg_human.set("HUMANO: 0")
 lbl_human = Label(root, textvariable=msg_human, font = ("Derive Unicode", 25), bg = 'white smoke').place(x=50,y=10)
 
-msg_maquina = StringVar()
-msg_maquina.set("MAQUINA: 0")
-lbl_maquina = Label(root, textvariable=msg_maquina, font = ("Derive Unicode", 25), bg = 'white smoke').place(x=650,y=10)
+msg_machine = StringVar()
+msg_machine.set("MAQUINA: 0")
+lbl_machine = Label(root, textvariable=msg_machine, font = ("Derive Unicode", 25), bg = 'white smoke').place(x=650,y=10)
 
 #Declaramos el lbl que indicara si gano o perdio el usario al momento de estar jugando
-msg_resultado = StringVar()
-lbl_resultado = Label(root, textvariable=msg_resultado, font = ("Derive Unicode", 25), bg = 'gray').place(x=362,y=120)
+msg_result = StringVar()
+lbl_result = Label(root, textvariable=msg_result, font = ("Derive Unicode", 25), bg = 'gray').place(x=362,y=120)
 
 #Declaramos el lbl que contendra la imagen seleccionada por el usuario
 img_human = ttk.Label(root, text="", background="gray")
